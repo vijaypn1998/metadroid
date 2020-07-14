0ed9c0506f40747140db56d14e934c1847544241
vijaypn1998 <67870989+vijaypn1998@users.noreply.github.com>
2020-07-14 21:44:43 +0530

    Update query.py

--- query.py
+++ query.py
@@ -1,23 +1,21 @@
 ﻿from rules import parser
-import parsley
-decoded_mql='publisher in ("Google", "VMware") AND "Creation Date" < -10m AND downloads > 1000 order by "Creation Date" DESC'
-a,b=parser(decoded_mql)
-
-query='''SELECT apk_id,title,creator,size
-FROM Apk
-WHERE'''
-space =" "
-for i in range(len(a)-1):
-    if(i!=len(a)-2)):
-        if(b[i][0]['operator']=='in'):
-            query = query + space + b[i][0]['column'] + space + b[i][0]['operator'] + space + '(' + b[i][0]['value'] + ")" + space + a[i]a[1]
+def query_generator(decoded_mql):
+    a,b=parser(decoded_mql)
+    query='''SELECT apk_id,title,creator,size
+    FROM Apk
+    WHERE'''
+    space =" "
+    for i in range(len(a)-1):
+        if(i!=len(a)-2)):
+            if(b[i][0]['operator']=='in'):
+                query = query + space + b[i][0]['column'] + space + b[i][0]['operator'] + space + '(' + b[i][0]['value'] + ")" + space + a[i]a[1]
+            else:
+                query = query + space + b[i+1][0]['column'] + space +b[i+1][0]['operator'] + space + b[i+1][0]['value'] + space a[i]a[1]
         else:
-            query = query + space + b[i+1][0]['column'] + space +b[i+1][0]['operator'] + space + b[i+1][0]['value'] + space a[i]a[1]
+            query = query + space + b[i+1][0]['column'] + space +b[i+1][0]['operator'] + space + b[i+1][0]['value']      
+    i=len(a)-2
+    if(a[i][1] == 'order by'):
+        query = query + '\n' + a[i][1] + space + b[i+1][0]['column'] + space + b[i+1][0]'[value'] 
     else:
-        query = query + space + b[i+1][0]['column'] + space +b[i+1][0]['operator'] + space + b[i+1][0]['value']  
-        
-i=len(a)-2
-if(a[i][1] == 'order by'):
-    query = query + '\n' + a[i][1] + space + b[i+1][0]['column'] + space + b[i+1][0]'[value'] 
-else:
-    query = query + space + b[i+1][0]['column'] + space +b[i+1][0]['operator'] + space + b[i+1][0]['value']
+        query = query + space + b[i+1][0]['column'] + space +b[i+1][0]['operator'] + space + b[i+1][0]['value']
+    return query
