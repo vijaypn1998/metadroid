db86fee97328d4a17361a23715963453b0593a5b
vijaypn1998 <67870989+vijaypn1998@users.noreply.github.com>
2020-07-14 21:38:31 +0530

    Update query.py

--- query.py
+++ query.py
@@ -5,11 +5,19 @@ a,b=parser(decoded_mql)
 
 query='''SELECT apk_id,title,creator,size
 FROM Apk
-WHERE '''
+WHERE'''
+space =" "
 for i in range(len(a)-1):
-    query = query + b[i]['column'] + b[i]['operator'] + b[i]['value'] + a[i]a[1]
+    if(i!=len(a)-2)):
+        if(b[i][0]['operator']=='in'):
+            query = query + space + b[i][0]['column'] + space + b[i][0]['operator'] + space + '(' + b[i][0]['value'] + ")" + space + a[i]a[1]
+        else:
+            query = query + space + b[i+1][0]['column'] + space +b[i+1][0]['operator'] + space + b[i+1][0]['value'] + space a[i]a[1]
+    else:
+        query = query + space + b[i+1][0]['column'] + space +b[i+1][0]['operator'] + space + b[i+1][0]['value']  
+        
 i=len(a)-2
 if(a[i][1] == 'order by'):
-    query = query + '\n' + a[i][1] + b[i+1]['column'] + b[i+1]'[value'] 
+    query = query + '\n' + a[i][1] + space + b[i+1][0]['column'] + space + b[i+1][0]'[value'] 
 else:
-    query = query + b[i+1]['column'] +b[i+1]['operator'] + b[i+1]['value']
+    query = query + space + b[i+1][0]['column'] + space +b[i+1][0]['operator'] + space + b[i+1][0]['value']
