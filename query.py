d4bedda643efb5cb34f0d557b3cf62a6b98f9b2c
vijaypn1998 <67870989+vijaypn1998@users.noreply.github.com>
2020-07-15 13:32:36 +0530

    Update query.py

--- query.py
+++ query.py
@@ -1,6 +1,6 @@
 import rules
 def generate_query(decoded_mql):
-    a, b = rules_2.parser(decoded_mql)
+    a, b = rules.parser(decoded_mql)
     mysql_query = '''SELECT apk_id,title,creator,size
     FROM apk
     WHERE'''
@@ -13,11 +13,20 @@ def generate_query(decoded_mql):
                 mysql_query = mysql_query + ' ' + b[i][0]['column'] + ' ' + b[i][0]['operator'] + ' ' + b[i][0][
                     'value'] + ' ' + a[i][1]
         else:
-            mysql_query = mysql_query + ' ' + b[i][0]['column'] + ' ' + b[i][0]['operator'] + ' ' + b[i][0]['value']
-
-    i = len(a) - 2
+            if (b[i][0]['operator'] == 'in'):
+                mysql_query = mysql_query + ' ' + b[i][0]['column'] + ' ' + b[i][0]['operator'] + ' (' + b[i][0][
+                    'value'] + ') '
+            else:
+                mysql_query = mysql_query + ' ' + b[i][0]['column'] + ' ' + b[i][0]['operator'] + ' ' + b[i][0][
+                    'value'] + ' '
+    i = len(a)-2
     if (a[i][1] == 'ORDER BY'):
          mysql_query = mysql_query + '\n' + a[i][1] + ' ' + b[i + 1][0]['column'] + ' ' + b[i + 1][0]['operator']
     else:
-        mysql_query = mysql_query + ' ' + b[i + 1][0]['column'] + ' ' + b[i + 1][0]['operator'] + ' ' + b[i + 1][0]['value']
+        if (b[i+1][0]['operator'] == 'in'):
+            mysql_query = mysql_query + ' ' + b[i+1][0]['column'] + ' ' + b[i+1][0]['operator'] + ' (' + b[i+1][0][
+                'value'] + ') '
+        else:
+            mysql_query = mysql_query + ' ' + b[i+1][0]['column'] + ' ' + b[i+1][0]['operator'] + ' ' + b[i+1][0][
+                'value']
     return mysql_query
