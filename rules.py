ceee6304a26a8aeb496c407ebaefaf13699413ad
vijaypn1998 <67870989+vijaypn1998@users.noreply.github.com>
2020-07-15 15:23:40 +0530

    Update rules.py

--- rules.py
+++ rules.py
@@ -27,25 +27,26 @@ def parser(decoded_mql):
     c = ','
     op = '('
     cp = ')'
-    operator = 'in' | '<' | '>' | 'DESC' | '=='
+    operator = 'in' | '<' | '>' | 'DESC' | '='
     p='publisher'
     space = ' '
-    words = words_1 space* words | words_1
+    words = words_1 space* words* | words_1
     words_1 = letterOrDigit+
     S = T
     T = <T1>:a ws 'AND' ws T->adder(a,"AND")
     T = <T1>:b ws 'OR' ws T->adder(b,"OR")
     T = <T1>:c ws 'order by' ws <X>:d->adder(d,None),adder(c,"ORDER BY")
     T = T1
-    T1 = q <words>:aa q  ws operator:oo ws op <U>:bb cp ->adder_2(aa,oo,bb) 
-    T1 = V 
-    T1 = W 
-    T1 = X
+    T1 = <letterOrDigit+>:aa  ws operator:oo ws op <U>:bb cp ->adder_2(aa,oo,bb)
+    T1 = q <words>:aa q ws operator:oo ws op <U>:bb cp -> adder_2(aa,oo,bb)
+    T1 = V
+    T1 = W
     U = U c ws U1 | U1
     U1 = q words q
-    V = q <words>:aa q ws <operator>:oo ws < q{0,1} letterOrDigit+ q{0,1}>:bb ->adder_2(aa,oo,bb) 
-    W = <words>:aa   ws <operator>:oo  ws <q{0,1} letterOrDigit+ q{0,1}>:bb ->adder_2(aa,oo,bb) 
+    V = q <words>:aa q ws <operator>:oo ws < q{0,1} letterOrDigit+ q{0,1}>:bb ->adder_2(aa,oo,bb)
+    W = <letterOrDigit+>:aa   ws <operator>:oo  ws <q{0,1} letterOrDigit+ q{0,1}>:bb ->adder_2(aa,oo,bb)
     X = q <words>:aa q ws <operator>:oo ->adder_2(aa,oo,None)
+    X = <letterOrDigit+>:aa ws <operator>:oo->adder_2(aa,oo,None)
     """
     x = parsley.makeGrammar(rules, {'adder': add,'adder_2': add_2})
     string = x(decoded_mql).S()
