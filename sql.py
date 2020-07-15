8943ebbc8120dadcf223453bb826e2adb7f7c8d3
vijaypn1998 <67870989+vijaypn1998@users.noreply.github.com>
2020-07-15 13:33:33 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,4 +1,4 @@
 from query import generate_query
-decoded_mql='"stfu stfu" in ("Google", "VMware") AND "Creation Date" < "11mm" AND downloads > abc order by "Creation Date" DESC'
+decoded_mql='"creator" in ("Google", "VMware") order by "Creation Date" DESC'
 mysql_query=generate_query(decoded_mql)
 print(mysql_query)
